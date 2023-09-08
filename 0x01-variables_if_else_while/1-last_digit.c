747184544e795754020002c4115f50518ffb03eb
Adagwen <ezeokaforadaobee@gmail.com>
2023-09-08 13:14:12 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -7,11 +7,13 @@
 
 /**
 
- * main - Determines if a number is positive, negative or zero.
+ * main - Prints the last digit of a randomly generated number
+
+ * and whether it is greater than 5, less than 6, or 0.
 
  *
 
- * Return: Always 0 (Success)
+ * Return: Always 0.
 
  */
 
@@ -26,19 +28,24 @@ int main(void)
 
         n = rand() - RAND_MAX / 2;
 
-        if (n > 0)
+
+        if ((n % 10) > 5)
 
         {
 
-                printf("%d is positive\n", n);
+                printf("Last digit of %d is %d and is greater than 5\n",
+
+                        n, n % 10);
 
         }
 
-        else if (n == 0)
+        else if ((n % 10) < 6 && (n % 10) != 0)
 
         {
 
-                printf("%d is zero\n", n);
+                printf("Last digit of %d is %d and is less than 6 and not 0\n",
+
+                        n, n % 10);
 
         }
 
@@ -46,13 +53,16 @@ int main(void)
 
         {
 
-                printf("%d is negative\n", n);
+                printf("Last digit of %d is %d and is 0\n",
+
+                        n, n % 10);
 
         }
 
+
         return (0);
 
-}
 
+}
 
 
