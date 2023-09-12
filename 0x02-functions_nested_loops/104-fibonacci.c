3feb1eb785314d32a3e677536cbdfe59dfa03b4c
Adagwen <ezeokaforadaobee@gmail.com>
2023-09-12 11:32:19 -0700

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -3,7 +3,7 @@
 
 /**
  * main _ The main function
- * 1 and 2, seperated by a comma
+ * 1 and 2, seperated by a comma followed by a space
  * Return: 0.
  */
 
@@ -14,7 +14,7 @@ int main(void)
 {
 	int count;
 	unsigned long fib1 = 0, fib2 = 1, sum;
-	unsigned long fib1_half1, fib2_half1, fib2_half2;
+	unsigned long fib1_half1, fib1_half2, fib2_half1, fib2_half2;
 	unsigned long half1, half2;
 
 
@@ -27,13 +27,13 @@ int main(void)
 	}
 	fib1_half1 = fib1 / 10000000000;
 	fib2_half1 = fib2 / 10000000000;
-	fib2_half2 = fib1 % 10000000000;
+	fib1_half2 = fib1 % 10000000000;
 	fib2_half2 = fib2 % 10000000000;
 	for (count = 93; count < 99; count++)
 	{
 	half1 = fib1_half1 + fib2_half1;
 	half2 = fib1_half2 + fib2_half2;
-	if (fib2_half2 + fib2_half2 > 9999999999)
+	if (fib1_half2 + fib2_half2 > 9999999999)
 	{
 	half1 += 1;
 	half2 %= 10000000000;
@@ -42,7 +42,7 @@ int main(void)
 	if (count != 98)
 	printf(", ");
 	fib1_half1 = fib2_half1;
-	fib1_half1 = fib2_half1;
+	fib1_half2 = fib2_half2;
 	fib2_half1 = half1;
 	fib2_half2 = half2;
 	}
