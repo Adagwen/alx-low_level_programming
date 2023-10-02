3c5ec0d428eb02629159043051ba0bd3d99c35b6
Adagwen <ezeokaforadaobee@gmail.com>
2023-10-02 07:07:07 -0700

    static libraries

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include<stdio.h>
+
+/**
+ * _puts - Prints a string to stdout.
+ * @str: The string to be printed.
+ *
+ * Description:
+ *  This function takes a string as input and prints it to the standard output.
+ *
+ * Return: void
+ */
+void _puts(char *str)
+{
+    /* Function implementation */
+}
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	_puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
